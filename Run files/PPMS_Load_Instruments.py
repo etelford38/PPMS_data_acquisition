b9cadb3ec67a1dca782c10b4400ec89bbfb8645b
etelford38 <120130958+etelford38@users.noreply.github.com>
2023-05-02 18:16:56 -0400

    Update PPMS_Load_Instruments.py

--- Run files/PPMS_Load_Instruments.py
+++ Run files/PPMS_Load_Instruments.py
@@ -1,3 +1,7 @@
+"""
+#Authors: Written by Maelle Kapfer (mak2294@columbia.edu) and modified by Jordan Pack (j.pack@columbia.edu), Evan Telford (ejt2133@columbia.edu), and Christie Koay (csk2172@columbia.edu)
+#Latest-update: April 27 2023
+"""
 from PyQt5.QtWidgets import (QApplication,  QDialog, 
                              QGridLayout, QVBoxLayout,  QLabel, 
                              QPushButton, QStyleFactory,  
