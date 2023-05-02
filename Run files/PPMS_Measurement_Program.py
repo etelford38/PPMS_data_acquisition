08765b8de66b45689cbca15aadc01c3715eb46b4
etelford38 <120130958+etelford38@users.noreply.github.com>
2023-05-02 18:17:06 -0400

    Update PPMS_Measurement_Program.py

--- Run files/PPMS_Measurement_Program.py
+++ Run files/PPMS_Measurement_Program.py
@@ -1,3 +1,7 @@
+"""
+#Authors: Written by Maelle Kapfer (mak2294@columbia.edu) and modified by Jordan Pack (j.pack@columbia.edu), Evan Telford (ejt2133@columbia.edu), and Christie Koay (csk2172@columbia.edu)
+#Latest-update: April 27 2023
+"""
 import os.path as osp
 import numpy as np
 from PyQt5.QtWidgets import (QApplication, QComboBox, QDialog, 
